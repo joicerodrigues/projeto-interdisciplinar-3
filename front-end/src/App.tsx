1d39dd42540663564a018bab7b789956191d5db4
Marcel de Menezes Araujo <89671847+araujomarcel@users.noreply.github.com>
2022-11-25 21:37:38 -0300

    adição do menu login
    adição do menu login

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import GlobalStyle from "./assets/css/global";
+import Cadastro from "./screens/Cadastro";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   );
