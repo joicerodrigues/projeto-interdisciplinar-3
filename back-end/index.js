0768cb6d6d464b08c8c50a40db6f4be0048647aa
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-12-09 18:35:19 -0300

    ✨ Adds login, session and product search by id

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,22 @@
 import {product} from './classProduct.js';
-import express from 'express';
+import express, { query } from 'express';
 import db from './databaseConnect.js';
 import JSONBig from 'json-bigint';
-import cors from 'cors';
+import session from 'express-session';
+import path from 'path';
+
 
 var router = express.Router();
 const server = express();
-server.use(express.json(), cors()); // faz com que o express entenda JSON
+server.use(express.json()); // faz com que o express entenda JSON
+
+
+server.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 
 // Query params = ?teste=1
 // Route params = /produtos/1
@@ -43,50 +53,51 @@ function checkProdutoInArray(req, res, next) {
   return next();
 }
 */
+// rota categoria
 
-//rotas vendedores
-server.get('/vendedores', async (req, res) =>{
- const result = await db.pool.query("select * from vendedor");
- res.send(result);
-});
-
-server.post('/vendedores', async (req, res) => {
-  let vendedor = req.body;
-
-  const result = await db.pool.query("insert into vendedor (id_usuario, nome, cnpj, nome_fantasia, email, contato, numero, rua, cep, bairro, tipo, cpf)values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [vendedor.id_usuario, vendedor.nome, vendedor.cnpj, vendedor.nome_fantasia, vendedor.email, vendedor.contato, vendedor.numero, vendedor.rua, vendedor.cep, vendedor.bairro, vendedor.tipo, vendedor.cpf]);
+server.get('/categoria', async (req, res) => {
+  const result = await db.pool.query("select * from categoria");
   res.send(JSONBig.parse(JSONBig.stringify(result)));
-});
+} );
 
-server.put('/vendedores/:id_vendedor', async (req, res) => {
-  let vendedor = req.body; 
-  let headerVendedor = req.params.id_vendedor; 
-  
-  const result = await db.pool.query("update vendedor set nome=?, cnpj=?, nome_fantasia=?, email=?, contato=?, numero=?, rua=?, cep=?, bairro=?, tipo=?, cpf=? where id_vendedor = ?", [vendedor.nome, vendedor.cnpj, vendedor.nome_fantasia, vendedor.email, vendedor.contato, vendedor.numero, vendedor.rua, vendedor.cep, vendedor.bairro, vendedor.tipo, vendedor.cpf, headerProduto]);
-  res.send(JSONBig.parse(JSONBig.stringify(result)));
-});
+server.get('/produtos_cat', async (req, res) => {
+  let categoria = req.body;
+  const result = await db.pool.query("select * from produto where id_categoria = ?", categoria.id_categoria);
+  res.send(result);
+} );
 
-server.delete('/vendedores/:id_vendedor', async (req, res) => {
-  let headerVendedor = req.params.id_vendedor; 
-  
-  const result = await db.pool.query("delete from vendedor set id_vendedor=? where id_vendedor = ?", [vendedor.id_vendedor, headerVendedor]);
-  res.send(JSONBig.parse(JSONBig.stringify(result)));
-}); // retorna os dados após exclusão
 
 // rotas produtos
 server.get('/produtos', async (req, res) => {
   const result = await db.pool.query("select * from produto");
 		res.send(result);
-    } );
+});
+
+server.get('/produtos/:id_produto', async (req, res) => {
+  let produto = req.params.id_produto; 
+  const result = await db.pool.query("select * from produto where id_produto = ?", produto);
+		res.send(result);
+});
+
+
+//listando produtos baseado no usuario conectado
+server.post('/produto_listar', async (req, res) => {
+  //let email = req.session.email;
+  let email = req.body.email;
+    const result = await db.pool.query("select * from produto where id_vendedor = (select id_vendedor from vendedor where email = ?)", email);
+    res.send(result); 
+});
+
  
 server.post('/produtos', async (req, res) => {
-  let produto = req.body; 
-  console.log(typeof produto.id_produto);
-  //const result = await db.pool.query("insert into produto values(?)", [produto.id_produto, produto.id_categoria, produto.id_vendedor, produto.nome, produto.valor, produto.peso, produto.imagem, produto.descricao])
-  const result = await db.pool.query("insert into produto (id_produto, id_categoria, id_vendedor, nome, valor, peso, imagem, descricao) values(?, ?, ?, ?, ?, ?, ?, ?)", [produto.id_produto, produto.id_categoria, produto.id_vendedor, produto.nome, produto.valor, produto.peso, produto.imagem, produto.descricao]);
+  //let email = req.session.email;
+  let email = req.body.email;
+  let produto = req.body; //sobrepõe/edita o index obtido ma rota de acordo com o novo valor
+  const response = await db.pool.query("select id_vendedor from vendedor where email = ?", email);
+  if(!!response && !!response[0].id_vendedor){
+  const result = await db.pool.query("insert into produto (id_categoria, id_vendedor, nome, valor, peso, imagem, descricao) values(?, ?, ?, ?, ?, ?, ?)", [produto.id_categoria, response[0].id_vendedor, produto.nome, produto.valor, produto.peso, produto.imagem, produto.descricao]);
   res.send(JSONBig.parse(JSONBig.stringify(result)));
- // const { Produto } = req.body; // assim esperamos buscar o name informado dentro do body da requisição  
- // produtos.push(Produto);
- // return res.json(produtos); // retorna a informação da variavel produtos
+  }
 });
 
 server.put('/produtos/:id_produto', async (req, res) => {
@@ -100,69 +111,98 @@ server.put('/produtos/:id_produto', async (req, res) => {
 server.delete('/produtos/:id_produto', async (req, res) => {
   let produto = req.body;
   let headerProduto = req.params.id_produto;
-
+  
   console.log(req.header.name);
   const result = await db.pool.query("delete from produto set id_produto=? where id_produto = ?", [produto.id_produto, headerProduto]);
   res.send(JSONBig.parse(JSONBig.stringify(result)));
- /* const { index } = req.params; // recupera o index com os dados
-  const { Produto } = req.body;
-  produtos[index] = Produto; // sobrepõe/edita o index obtido na rota de acordo com o novo valor
-  return res.json(produtos);*/
-}); // retorna novamente os produtos atualizados após o update
-
-server.delete('/produtos/:index', checkProdutoInArray, (req, res) => {
-  const { index } = req.params; // recupera o index com os dados
-
-  produtos.splice(index, 1); // percorre o vetor até o index selecionado e deleta uma posição no array
-
-  return res.send();
 }); // retorna os dados após exclusão
 
+
+//rotas vendedores
+server.get('/vendedores/:id_user', async (req, res) =>{
+  let vendedor = req.body; 
+  let headerVendedor = req.params.id_vendedor; 
+  const result = await db.pool.query("select * from vendedor where id_user = ?",[headerVendedor]);
+  res.send(result);
+ });
+ 
+ server.post('/vendedores', async (req, res) => {
+   let vendedor = req.body; //let = "variável local no escopo do bloco atual".
+   let email = req.body.email
+   let query = "SELECT email FROM vendedor WHERE email = ?";
+
+   const hasEmail = await db.pool.query(query, [email]);
+   console.log(req.hasEmail);
+   if(email != ""){
+   if (hasEmail.length === 0){
+    const result = await db.pool.query("insert into vendedor (nome,cnpj, nome_fantasia, contato, numero, rua, cep, bairro, cpf, email, cidade, uf, senha)values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [vendedor.nome, vendedor.cnpj, vendedor.nome_fantasia, vendedor.contato, vendedor.numero, vendedor.rua, vendedor.cep, vendedor.bairro, vendedor.cpf, vendedor.email, vendedor.cidade, vendedor.uf, vendedor.senha]);
+    res.send(JSONBig.parse(JSONBig.stringify(result)));
+   } else{
+    res.send('Email já cadastrado!');
+   }
+ }
+ });
+ 
+ server.put('/vendedores/:id_vendedor', async (req, res) => {
+   let vendedor = req.body; 
+   let headerVendedor = req.params.id_vendedor; 
+   
+   const result = await db.pool.query("update vendedor set nome=?, cnpj=?, nome_fantasia=?, email=?, contato=?, numero=?, rua=?, cep=?, bairro=?, cpf=?, cidade=?, uf=?, where id_vendedor = ?", [vendedor.nome, vendedor.cnpj, vendedor.nome_fantasia, vendedor.email, vendedor.contato, vendedor.numero, vendedor.rua, vendedor.cep, vendedor.bairro, vendedor.tipo, vendedor.cpf, vendedor.cidade, vendedor.uf, headerProduto]);
+   res.send(JSONBig.parse(JSONBig.stringify(result)));
+ });
+ 
+ server.delete('/vendedores/:id_vendedor', async (req, res) => {
+   let headerVendedor = req.params.id_vendedor; 
+   
+   const result = await db.pool.query("delete from vendedor set id_vendedor=? where id_vendedor = ?", [vendedor.id_vendedor, headerVendedor]);
+   res.send(JSONBig.parse(JSONBig.stringify(result)));
+ }); // retorna os dados após exclusão
+ 
+
 // rotas login
-server.get('/login', async (req, res) => {
-	try {
-		const result = await db.pool.query("select * from usuario");
-		res.send(result);
-	} catch (err) {
-		throw err;
+
+server.post('/login' , async  (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let query = "SELECT * FROM vendedor WHERE email = ? AND senha = ?";
+  if(email != "" && password != ""){
+    const result =  await db.pool.query(query, [email, password]);
+			// caso email e senha forem corretos e existentes
+			if (result.length > 0) {
+				// autentica o usuario
+				req.session.loggedin = true;
+				req.session.email = email;
+        res.send('Gloria conectou Aleluia');
+			} else { 
+				res.send('Email ou Senha estão incorretos!');
+			}		
+      	
+			res.end();
+	} else {
+		res.send('Por favor Digite seu email e senha!');
+		res.end();
 	}
-  });
+});
 
-  server.post('/login', function(req, res) {
 
-    let email = req.body.email;
-    let password = req.body.password;
-    
-    if (email && password) {
-      
-      db.query('SELECT FROM usuario WHERE email = ? AND senha = ?', [email, password], function(error, results, fields) {
-  
-        if (error) throw error;
-    
-        if (results.length > 0) {
-          
-          req.session.loggedin = true;
-          req.session.email = email;
-          
-          res.redirect('/home');
-        } else {
-          res.send('Incorrect email and/or Password!');
-        }			
-        res.end();
-      });
-    } else {
-      res.send('Please enter email and Password!');
-      res.end();
-    }
-  });
-  
   server.get('/home', function(req, res) {
     
     if (req.session.loggedin) {
+      //caso esteja logado
       res.send('Welcome back, ' + req.session.email + '!');
     } else {
-      res.send('Please login to view this page!');
+      //caso nao esteja logado
+      res.send('Esteja conectado para visualizar está pagina');
     }
     res.end();
   });
+
+  server.get('/logout', function(request, response, next){
+    //destruindo a session e redericionando para home
+    req.session.destroy();
+    response.redirect("/");
+
+});
+
+
 server.listen(3000);
